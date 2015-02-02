dc31ea013c2af6b5069d397addfb3828234532fe
Kevin Hatfield <khatfield@socllc.net>
2015-02-02 16:37:33 -0600

    Create edgeos.inc.php

---
+++ edgeos.inc.php
@@ -0,0 +1,21 @@
+<?php
+
+/**
+ * Observium
+ *
+ *   This file is part of Observium.
+ *
+ * @package    observium
+ * @subpackage discovery
+ * @copyright  (C) 2006-2014 Adam Armstrong
+ *
+ * Patched for EdgeMax implementation : 02/02/2015
+ * Kevin Hatfield - khatfield@socllc.net
+ */
+
+if (!$os)
+{
+  if (preg_match("/^Edge(Max|OS|Switch)/", $sysDescr)) { $os = "edgeos"; }
+}
+
+// EOF
